a99b2449095ab1465ee4dbfe7e0bba197266e123
CodeBlanc <kyotaka29@gmail.com>
2023-09-28 23:45:03 +0700

    initial commit

---
+++ Jurnal01.java
@@ -0,0 +1,8 @@
+public class Jurnal01 {
+    public static void main(String[] args) {
+        System.out.println("Nama: Rafi Kusumahhadi");
+        System.out.println("NIM: 607062330076");
+        System.out.println("kelas: D3IF-47-03");
+    }
+    
+}
